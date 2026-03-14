3a74b7e760246d67ec19e780f195945aebd7f806
Kevin Xiao <xlzqwerty1@gmail.com>
2026-03-14 03:27:28 -0400

    Support var-as-path expansion

--- functions/_fits_expand_tilde.fish
+++ functions/_fits_expand_tilde.fish
@@ -1,3 +1,12 @@
-function _fits_expand_tilde --description "Expand ~ to \$HOME in a path"
-    string replace -r '^~' "$HOME" -- $argv[1]
+function _fits_expand_tilde --description "Expand ~ and \$VAR in a token"
+    set -l s (string replace -r '^~' "$HOME" -- $argv[1])
+    while set -l m (string match -r '^(.*?)\$(\w+)(.*)$' -- $s)
+        set -l varname $m[3]
+        if set -q $varname
+            set s "$m[2]$$varname$m[4]"
+        else
+            break
+        end
+    end
+    echo $s
 end
